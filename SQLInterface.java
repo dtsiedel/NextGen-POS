465b0be9524955270596f16e069688b81739062b
Drew Siedel <dts.siedel@gmail.com>
2015-10-18 18:53:28 -0400

    Added an addProduct() function
    The function is a tool that will allow for an initializeDatabase() function in the future, setting up the database at the beginning of execution. Currently using it to populate my local database for testing.

--- SQLInterface.java
+++ SQLInterface.java
@@ -4,22 +4,13 @@ import java.util.*;
 public class SQLInterface
 {
 	//given an id number, prints to stdout relevant information
-	public static String getInfo(String id) throws InterruptedException, IOException
+	public static String getInfo(int id) throws InterruptedException, IOException
 	{
 
-		if(id.matches("^\\d+$")) //if the input is a number
-		{
-			//intentionally blank
-		}
-		else
-		{
-			System.out.println("Invalid Input! Please enter a number next time.");
-			System.exit(0);
-		}
 
 		//call sqlite command to access database
 		String[] command = {"sqlite3", "products", "SELECT * FROM products WHERE id = "};
-		command[2] = command[2] + id + ";";
+		command[2] = command[2] + Integer.toString(id) + ";";
 		ProcessBuilder pb = new ProcessBuilder(command);
 
 
@@ -40,7 +31,7 @@ public class SQLInterface
 
 		if(f.length() == 0) //if there are no lines in the output, item not found
 		{
-			System.out.println("Item not found");
+			System.out.println("Item not found, ensure you entered the ID properly and try again.");
 			deleter.start(); ///delete output.txt
 			System.exit(0);
 		}
@@ -54,10 +45,11 @@ public class SQLInterface
 	}
 
 	//returns just the price based on id
-	public static Double getPrice(String id) throws InterruptedException, IOException
+	public static Double getPrice(int id) throws InterruptedException, IOException
 	{
 		String detailString = getInfo(id);
 
+		//weird workaround because Java's split method doesn't work on "|" character
 		String adjustedString = detailString.replace("|", "~");
 
 		String[] vals = adjustedString.split("~");
@@ -67,7 +59,7 @@ public class SQLInterface
 	}
 
 	//returns the name of the product based on id
-	public static String getProductName(String id) throws InterruptedException, IOException
+	public static String getProductName(int id) throws InterruptedException, IOException
 	{
 		String detailString = getInfo(id);
 
@@ -79,13 +71,34 @@ public class SQLInterface
 	}
 
 
-	//test stub 
+	//allows for the insertion of a new item into the database
+	//primarily used as a utility function by the database initialization function
+	//it is (currently) the responsibility of the client function to ensure that an id is not occupied before executing this command
+	public static void addProduct(int id, String name, Double price) throws InterruptedException, IOException
+	{
+		String base = "INSERT INTO products VALUES("; //base command to insert
+		//then add appropriate parameters
+		String insertCommand = base + Integer.toString(id) + ",\"" + name + "\"," + Double.toString(price) + ");";
+		
+		String[] commands = {"sqlite3", "products", insertCommand};
+
+
+		ProcessBuilder pb = new ProcessBuilder(commands);
+		Process p = pb.start();
+		p.waitFor();
+
+
+	}
+
+
+	//test stub, just runs through the functions
 	public static void main(String[] args) throws InterruptedException, IOException
 	{
+		//test basic accessing of fields
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Enter the ID of the product you wish to learn about: ");
-		String id = sc.next();
+		int id = sc.nextInt();
 
 		Double price = getPrice(id);
 		System.out.println("Price: " + price);
@@ -93,7 +106,29 @@ public class SQLInterface
 		String name = getProductName(id);
 		System.out.println("Name: " + name);
 
-	}
 
+		//test adding an item
+		System.out.print("Add a product? ");
+		char cont = sc.next().charAt(0);
+
+		if((cont == 'y') || (cont == 'Y'))
+		{
+			System.out.print("ID: ");
+			id = sc.nextInt();
+
+			System.out.print("Item name: ");
+			name = sc.next();
+
+			System.out.print("Price: ");
+			price = Double.parseDouble(sc.next());
+
+			addProduct(id, name, price);
+
+			System.out.println("Price of " + name + ": " + getPrice(id));
+
+		}
+
+
+	}
 
 }
