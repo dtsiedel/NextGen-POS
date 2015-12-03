230f0a67a017d791af652084ee8aaa3166a88a08
sudonotpseudo <sudonotpseudo@gmail.com>
2015-12-03 17:44:25 -0500

    Transaction.java updated

--- Transaction.java
+++ Transaction.java
@@ -57,14 +57,18 @@ public class Transaction extends Register {
                             continue;
                         }
                         System.out.print("Enter an item to remove\n-->");
+                        if (!transaction.hasNextInt()){throw new InputMismatchException();}
                         currentCart.removeItem(transaction.nextInt()); //read in another value to remove that item
                     } else if (input == cancelTransaction) { //input is -190
                         cancelTransaction(changes);
                         Cashier.cashierDo();
                         break;
+                    } else if (input >= SQLInterface.getInstance().getMaxID() || input <= 0){
+                        System.out.println("Error, Item not found");
                     } else { //input is none of the options, thus possibly a valid itemNumber to add an item to cart
                         //based on input, return Item from database called item
                         System.out.print("Enter quantity of item to be purchased\n-->"); //prompt user to enter quantity of items to buy
+                        if (!transaction.hasNextInt()){throw new InputMismatchException();}
                         int itemQuan = transaction.nextInt();
                         //if the item entered is a rental prompt them to start rental process
                         if (SQLInterface.getInstance().isRentable(input)) {
@@ -103,9 +107,13 @@ public class Transaction extends Register {
                     }
                 } else {
                     System.out.println("INVALID INPUT...Try Again");
+                    System.out.println();
+                    transaction.nextLine();                
                 }
-            } catch (NumberFormatException e) {
+            } catch (NumberFormatException| InputMismatchException e) {
                 System.out.println("Error reading input, try again");
+                System.out.println();
+                transaction.nextLine();
             }
             System.out.printf("\nCurrent Cart Subtotal : %5.2f", this.currentCart.getSubtotal());
             System.out.print("\n\n");
@@ -116,48 +124,57 @@ public class Transaction extends Register {
         int pt = getPaymentType();
 
         if (registerPay(pt)) {
-            if (pt == 0) { //payment type is cash, run through getting cash, print receipt etc.
-                Scanner cashIn = new Scanner(System.in);
-                System.out.printf("Cart Total: %6.2f\n", (this.currentCart.getSubtotal()*1.06));
-                System.out.print("Enter cash recieved\n-->"); //should put this in a loop, make another method?
-                double c = 0.0;
-                if (cashIn.hasNextDouble()) {
-                    c = cashIn.nextDouble();
-                }
-                double change = makeChange(c, currentCart.getSubtotal() + tax);
-                Receipt receipt = new Receipt(currentCart, tax, pt);
-                receipt.store();
-                receipt.print();
-                System.out.printf("Your change is %6.2f.\n", change);
-            } 
-            else if (pt == 1) 
-            {
-                boolean valid = false;
-                do
-                {
-                    valid = false;
-                    Scanner creditCardScan = new Scanner(System.in);
-                    System.out.print("Enter credit card number\n-->");
-                    if (creditCardScan.hasNextLong()) 
-                    {
-                        long ccN = creditCardScan.nextLong();
-                        String ccNString = Long.toString(ccN);
-                        boolean validate = CreditCheck.getInstance().creditCardCheck(ccNString);
-                        if (validate) 
-                        { //valid cc
-                            System.out.println("Valid Credit Card Entered");
-                            valid = true;
-                            Receipt receipt = new Receipt(currentCart, tax, pt);
-                            receipt.store();
-                            receipt.print();
-                        } 
-                        else 
-                        {
-                            System.out.println("Invalid Credit Card Entered, Try Again"); //not 100% sure where this will end up afterwards, need to check and adjust
-                        }
+            boolean paid = false;
+            while(paid){
+                if (pt == 0) { //payment type is cash, run through getting cash, print receipt etc.
+                    Scanner cashIn = new Scanner(System.in);
+                    System.out.printf("Cart Total: %6.2f\n", (this.currentCart.getSubtotal()*1.06));
+                    System.out.print("Enter cash recieved\n-->"); //should put this in a loop, make another method?
+                    double c = 0.0;
+                    if (cashIn.hasNextDouble()) {
+                        c = cashIn.nextDouble();
                     }
-                }while(!valid);
-            }
+                    double change = makeChange(c, currentCart.getSubtotal() + tax);
+                    Receipt receipt = new Receipt(currentCart, tax, pt);
+                    receipt.store();
+                    receipt.print();
+                    System.out.printf("Your change is %6.2f.\n", change);
+                    paid = true;
+                } 
+                else if (pt == 1) 
+                {
+                    boolean valid = false;
+                    do
+                    {
+                        valid = false;
+                        Scanner creditCardScan = new Scanner(System.in);
+                        System.out.print("Enter credit card number or enter -999\n-->");
+                        if (creditCardScan.hasNextLong()) 
+                        {
+                            long ccN = creditCardScan.nextLong();
+                            if (ccN.equals(-999)){
+                                pt = 0;
+                                break;
+                            }
+                            String ccNString = Long.toString(ccN);
+                            boolean validate = CreditCheck.getInstance().creditCardCheck(ccNString);
+                            if (validate) 
+                            { //valid cc
+                                System.out.println("Valid Credit Card Entered");
+                                valid = true;
+                                Receipt receipt = new Receipt(currentCart, tax, pt);
+                                receipt.store();
+                                receipt.print();
+                                paid = true;
+                            } 
+                            else 
+                            {
+                                System.out.println("Invalid Credit Card Entered, Try Again"); //not 100% sure where this will end up afterwards, need to check and adjust
+                            }
+                        }
+                    }while(!valid);
+                }
+        }
         }
     }
 
